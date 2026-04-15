1df4b64a6e9f3c4a45cc7a30941303aa8384a8a4
SStatZX <lewis.greasley@gmail.com>
2026-04-15 22:58:13 +0100

    Changed class to be Serializable

--- app/src/main/java/uk/edu/le/co2124/part2/database/SafetyCheckWithDefects.java
+++ app/src/main/java/uk/edu/le/co2124/part2/database/SafetyCheckWithDefects.java
@@ -3,9 +3,11 @@ package uk.edu.le.co2124.part2.database;
 import androidx.room.Embedded;
 import androidx.room.Relation;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class SafetyCheckWithDefects {
+// Class implements Serializable so it can be put as an extra on line ~46 of MainActivity, shouldn't break anything
+public class SafetyCheckWithDefects implements Serializable {
     @Embedded
     public SafetyCheck safetyCheck;
 
