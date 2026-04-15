2ff47d103c19cdcef8629d60af59a7b3e7d667c4
SStatZX <lewis.greasley@gmail.com>
2026-04-15 20:31:04 +0100

    Added initial detail view files

---
+++ app/src/main/java/uk/edu/le/co2124/part2/DetailActivity.java
@@ -0,0 +1,14 @@
+package uk.edu.le.co2124.part2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
